425a03f45ac86c2326444a94da7e795314f0b076
jbaldwin0 <jac0b67@ymail.com>
2022-08-12 15:33:00 -0500

    Add capability to perform multiple operations at once

--- js/script.js
+++ js/script.js
@@ -15,7 +15,6 @@ function divide(a, b) {
 }
 
 function operate(op, a, b) {
-    console.log(op+" "+a+" "+b);
     if (op == "+") {
         return add(a, b);
     } else if (op == "-") {
@@ -37,19 +36,41 @@ const buttons = document.querySelectorAll(".num").forEach(e => {
 });
 
 function useCalc (e) {
-    console.log(e.textContent);
     if (e.textContent == "Clear") {
-        inDisplay.textContent = "0";   
+        inDisplay.textContent = "0";
+        outDisplay.textContent = "0";  
     }
     else if (e.textContent == "=") {
         let str = inDisplay.textContent;
-        let nums = str.split(/\D/);
-        let operand = str.split(/[0-9]+/);
-        let num1 = nums[0];
+        let nums = str.split(/\D/g);
+        console.log(nums);
+        let operand = str.split(/[0-9]+/g);
+        console.log(operand);
+       /* let num1 = nums[0];
         let num2 = nums[1];
-        let op = operand[1];
-        console.log(operate(op, num1, num2))
-        outDisplay.textContent = operate(op, num1, num2);
+        let op = operand[1]; */
+        let num1, num2, op;
+        let result = 0;
+        for (let i = 1; i < operand.length-1; i++) {
+            if (i == 1) {
+                num1 = nums.shift();
+                num2 = nums.shift();
+            }
+            else {
+                num1 = result;
+                num2 = nums.shift();
+            }
+            op = operand[i];
+            console.log(op+" "+num1+" "+num2);
+            result = operate(op, num1, num2);                   
+        }
+        if (result == NaN || result == undefined) {
+            outDisplay.textContent = "ERROR";
+        }
+        else {
+            console.log(result); 
+            outDisplay.textContent = result;
+        }
     }    
     else if (inDisplay.textContent != "0"){
         inDisplay.append(e.textContent);
